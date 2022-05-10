a2a34e4cbcea1c23e40be4ee590b278b2b6b03d9
Ginthom <thomas@gingele.de>
2022-05-10 12:59:20 +0200

    Enable broadcast

--- udp_socket.cpp
+++ udp_socket.cpp
@@ -7,16 +7,21 @@
 
 using boost::asio::ip::udp;
 
+const int PORT = 5555;
+
 void send_udp(std::string message) {
 	try {
 		boost::asio::io_context io_context;
 		udp::resolver resolver(io_context);
 		boost::array<char, 1> recv_buf;
+
 		udp::endpoint remote_endpoint;
-		udp::socket socket(io_context, udp::endpoint(udp::v4(), 13));
-		socket.receive_from(boost::asio::buffer(recv_buf), remote_endpoint);
-		boost::system::error_code ignored_error;
-		socket.send_to(boost::asio::buffer(message), remote_endpoint, 0, ignored_error);
+		udp::socket socket(io_context, udp::endpoint(udp::v4(), PORT));
+
+		boost::asio::socket_base::broadcast option(true);
+                socket.set_option(option);
+
+		socket.send(boost::asio::buffer(message));
 	} catch(std::exception &e) {
 		std::cout << "An error occured: " << e.what() << std::endl;
 	}
