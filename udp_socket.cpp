8878f838d25b6faa8d2475c000133ed63a44fc00
Ginthom <thomas@gingele.de>
2022-05-12 08:11:32 +0200

    New test version

--- udp_socket.cpp
+++ udp_socket.cpp
@@ -14,15 +14,8 @@ void send_udp(std::string message) {
 		boost::asio::io_service iosrv;
 		udp::socket sock(iosrv, udp::endpoint(udp::v4(), PORT));
 		sock.set_option(boost::asio::socket_base::broadcast(true));
-		udp::endpoint dest(udp::v4(), PORT);
-
-		for (;;) {
-			std::string strbuf;
-			udp::endpoint sender_endpoint;
-			sock.receive_from(boost::asio::buffer(strbuf, strbuf.size()), sender_endpoint);
-			sock.send_to(boost::asio::buffer(message, sizeof(std::string)), sender_endpoint);
-		}
-
+		udp::endpoint dest(boost::asio::ip::address::from_string("192.168.178.255"), PORT);
+		sock.send_to(boost::asio::buffer(message, sizeof(std::string)), dest);
 		sock.close();
 
 	} catch(std::exception &e) {
@@ -31,5 +24,5 @@ void send_udp(std::string message) {
 }
 
 int main() {
-	send_udp("Hallo Felix");
+	send_udp("Hallo ihr alle, esst mein braodcast signal.");
 }
